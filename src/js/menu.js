b7c58547523995a4f4139b59618174eccf909162
supert111 <sportsmen@bigmir.net>
2021-06-29 12:11:48 +0300

    team-project

---
+++ src/js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    header: document.querySelector('header')
+  };
+  function toggleModal(e) {
+    if(e.target.closest('button') === refs.openModalBtn || e.target.closest('button') === refs.closeModalBtn){
+      refs.modal.classList.toggle('is-hidden');
+    } else if (e.target.href){
+      refs.modal.classList.add('is-hidden')
+    }
+  }
+  document.addEventListener('click', toggleModal);
+})();
